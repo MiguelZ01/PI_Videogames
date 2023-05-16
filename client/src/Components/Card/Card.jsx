df963b7dcb6ddefba88ea05e57b92abcd041eaaf
MiguelZ01 <jesusmigu3lzr2004@gmail.com>
2023-05-15 22:04:56 -0500

    9th commit

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -1,10 +1,16 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { videogameGET } from '../../redux/actions/action'
 import { NavLink } from 'react-router-dom';
 import style from './Card.module.css';
+import Paginate from "../Paginate/Paginate";
 
 const Card = () => {
+    const [pagina, setPagina] = useState(1);
+    const [porPagina, setPorPagina] = useState(6)
+
+    const maximo = videogameGET.length / porPagina;
+
     const dispatch = useDispatch();
 
     const Allvideogames = useSelector((state) => state.videogames)
@@ -16,17 +22,20 @@ const Card = () => {
     return (
         <div>
             {
-                Allvideogames.map(({ id, name, imagen, rating }) => {
-                    return (
-                        <div className={style.principal} key={id}>
-                            <NavLink to={`/detail/${id}`}><h3>{name}</h3></NavLink>
-                            <h3>{id}</h3>
-                            <h3>{rating}</h3>
-                            <img src={imagen} alt="image not found" className={style.imagen} />
-                        </div>
-                    )
-                })
+                Allvideogames
+                    .slice((pagina - 1) * porPagina, (pagina - 1) * porPagina + porPagina)
+                    .map(({ id, name, imagen, rating }) => {
+                        return (
+                            <div className={style.principal} key={id}>
+                                <NavLink to={`/detail/${id}`}><h3>{name}</h3></NavLink>
+                                <h3>{id}</h3>
+                                <h3>{rating}</h3>
+                                <img src={imagen} alt="image not found" className={style.imagen} />
+                            </div>
+                        )
+                    })
             }
+            <Paginate pagina={pagina} setPagina={setPagina} maximo={maximo} />
         </div >
     )
 }
--- client/src/Components/views/Home/Home.jsx
+++ client/src/Components/views/Home/Home.jsx
@@ -2,22 +2,15 @@ import { useState } from "react";
 import style from './Home.module.css';
 import Navbar from '../../Navbar/Navbar';
 import Card from '../../Card/Card';
-import Paginate from "../../Paginate/Paginate";
-import { videogameGET } from '../../../redux/actions/action'
 
 const Home = () => {
-
-  const [pagina, setPagina] = useState(1);
-  const [porPagina, setPorPagina] = useState(3)
-
-  const maximo = videogameGET.length / porPagina
 
   return (
     <div className={style.principal}>
       <div className={style.search}>
         <Navbar />
         <Card />
-        <Paginate pagina={pagina} setPagina={setPagina} maximo={maximo} />
+
       </div>
     </div>
   )
