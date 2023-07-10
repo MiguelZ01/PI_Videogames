e37bd825a2e2db5f849eb81f93af5ec7094c5034
MiguelZ01 <jesusmigu3lzr2004@gmail.com>
2023-07-09 19:43:03 -0500

    Deploy

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const PORT = process.env;
+const PORT = process.env.PORT;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
